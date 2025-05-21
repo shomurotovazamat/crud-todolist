4b2f7b2019387d058fbab42db1089ba159d9e504
shomurotovazamat <shomuratov16@gmail.com>
2025-05-21 19:59:00 +0500

    crud tayyor

--- index.js
+++ index.js
@@ -5,19 +5,39 @@ const app = express();
 app.use(express.json());
 
 app.post('/', (req, res) => {
-
+    const task = {
+        id: Date.now(),
+        text: req.body.text,
+        done: false
+    }
+    res.status(201).send(task);
 })
 
 app.get('/', (req, res) => {
-
+    const task = [
+        {
+            id: Date.now(),
+            text: 'Hello task todo list',
+            done: false
+        },
+    ]
+    res.status(200).send(task);
 })
 
 
+app.put('/:id', (req, res) => {
+    const task = {
+        id: req.params.id,
+        text: req.body.text,
+        done: req.body.done
+    }
+    res.status(200).send(task);
+})
 
 
-
-
-
+app.delete('/:id', (req, res) => {
+    res.sendStatus(205)
+})
 
 
 app.listen(8000)
