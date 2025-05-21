a65a519a26c8478aa510eeb46c9996cbf2cd0aa5
shomurotovazamat <shomuratov16@gmail.com>
2025-05-21 17:48:48 +0500

    endpoint create

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+
+})
+
+app.get('/', (req, res) => {
+
+})
+
+
+
+
+
+
+
+
+
+app.listen(8000)
